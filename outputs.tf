b5823b4054f93bcc881ed3be37a002c23ce94ce9
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-06-30 17:38:44 +0200

    Output credentials

--- outputs.tf
+++ outputs.tf
@@ -3,18 +3,8 @@ output "service_id" {
   value       = cloudfoundry_service_instance.redis.id
 }
 
-output "metrics_host" {
-  description = "The exporter metrics internal hostname"
-  //noinspection HILUnresolvedReference
-  value = join("", cloudfoundry_route.exporter.*.endpoint)
-}
-
-output "metrics_port" {
-  description = "The exporter metrics internal port"
-  value       = "9121"
-}
-
-output "metrics_app_id" {
-  description = "The metrics app ID"
-  value       = cloudfoundry_app.exporter.id
+output "credentials" {
+  description = "The service credentials"
+  sensitive   = true
+  value       = cloudfoundry_service_key.key.credentials
 }
